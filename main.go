0fd15dc99fd71c21163a0a81b6cd72c63679ba15
Robert Carlsen <rwcarlsen@gmail.com>
2012-02-28 14:17:41 -0600

    can read and structure google ngram data now - line by line.

--- main.go
+++ main.go
@@ -3,24 +3,20 @@ package main
 
 import (
   "io/ioutil"
+  "strconv"
+  "strings"
   "fmt"
+  "os"
+  "bufio"
   gosql "github.com/kuroneko/gosqlite3"
   "net/http"
 )
 
 func main() {
-  loadData()
-
   fmt.Println("Starting http server...")
   indexHandler := staticFileHandler("index.html")
-  errHandler := staticFileHandler("error.html")
-  dataHandler := func(w http.ResponseWriter, req *http.Request) {
-    fmt.Println("New Request")
-    _, _ = w.Write([]byte("data-retrieval"))
-  }
 
   http.HandleFunc("/", indexHandler)
-  http.HandleFunc("/invalid-page/", errHandler)
   http.HandleFunc("/data/", dataHandler)
   err := http.ListenAndServe("0.0.0.0:8888", nil)
   if err != nil {
@@ -38,8 +34,82 @@ func staticFileHandler(file_name string) func(http.ResponseWriter,
   }
 }
 
-func loadData() {
+func dataHandler(w http.ResponseWriter, req *http.Request) {
+  path := req.URL.Path
+  fmt.Fprintln(w, path)
+
+  if path == "/data/set1" {
+    words := loadWordData("/home/robert/grams1.csv")
+    for _, elem := range words {
+      fmt.Fprintln(w, &elem)
+    }
+  } else if path == "/data/set2" {
+    words := loadWordData("/home/robert/grams2.csv")
+    for _, elem := range words {
+      fmt.Fprintln(w, &elem)
+    }
+  } else {
+    myword := &Word{Text:"foo",Year:1980, BookCount:4, PageCount:56, Count:81}
+    fmt.Fprintln(w, myword)
+    fmt.Fprintln(w, myword.PageDensity())
+  }
+}
+
+func loadWordData(file_name string) []Word {
+  var words []Word
+
+  file, err := os.Open(file_name)
+  if err != nil {
+    fmt.Println("Error: ", err)
+    return words
+  }
+
+  reader := bufio.NewReader(file)
+  max_words := 1000000
+  for i := 0; i < max_words; i++ {
+    line, _, err2 := reader.ReadLine()
+    if err != nil {
+      fmt.Println("Error: ", err2)
+      break
+    }
+    pieces := strings.Split(string(line), "\t")
+
+    wordText := pieces[0]
+    year, _ := strconv.Atoi(pieces[1])
+    count, _ := strconv.Atoi(pieces[2])
+    pageCount, _ := strconv.Atoi(pieces[3])
+    bookCount, _ := strconv.Atoi(pieces[4])
+
+    myword := Word{wordText, year, count, pageCount, bookCount}
+    words = append(words, myword)
+  }
+  return words
+}
+
+type Word struct {
+  Text string
+  Year int
+  Count int
+  PageCount int
+  BookCount int
+}
+
+func (w *Word) PageDensity() float32 {
+  return float32(w.Count) / float32(w.PageCount)
+}
+
+func (w *Word) String() string {
+  str := w.Text
+  str += " {BookCount = " + strconv.Itoa(w.BookCount)
+  str += ", PageCount = " + strconv.Itoa(w.PageCount)
+  str += ", Count = " + strconv.Itoa(w.Count)
+  str += ", Year = " + strconv.Itoa(w.Year)
+  str+= "}"
+  return str
+}
+
+func loadSqliteData() {
   db, _ := gosql.Open("/home/robert/cycout/cyclus.sqlite")
   fmt.Println(db)
-
 }
+
