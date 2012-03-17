ecabd6b13fc2fccc45bfe0e94310e0bbd9226cc6
Eric Alexander <ealexand@cs.wisc.edu>
2012-03-16 19:20:21 -0500

    Figured out lines (finally). Lots of junk comments I'll clean up.

--- viz.js
+++ viz.js
@@ -3,8 +3,10 @@ var h = 750; //height
 var w = 1200; //width
 var r = 3; //radius - IS THIS EVER USED?
 var rbig = 8; //max radius size - JUST SEEMS TO BE USED TO DEFINE PAD
-//var pad = rbig + 10 //padding around the graphing space
-var pad = 50
+var pad = rbig + 10 //padding around the graphing space
+var xOffset = 40
+var yOffset = 40
+//var pad = 300
 
 var data = [];
 var num_datums = 500;
@@ -25,6 +27,38 @@ var viz = d3.select("#viz")
   .append("svg:svg")
   .attr("width", w)
   .attr("height", h);
+
+/*var tmpData = [1,2,3,4,5]
+var testh = 200
+var testw = 500
+var x = d3.scale.linear().domain([0, tmpData.length - 1]).range [0, testw]
+var y = d3.scale.linear().domain([0, d3.max(tmpData)]).range [testh, 0]
+
+var testVis = d3.select("#test")
+  .append("svg:svg")
+  .attr("width",testw)
+  .attr("height",testh);
+
+testVis.append("svg:rect")
+  .attr("x",50)
+  .attr("y",50)
+  .attr("height",50)
+  .attr("width",50);
+  
+viz.append("svg:line")
+  .attr("x1",25)
+  .attr("y1",25)
+  .attr("x2",150)
+  .attr("y2",25)
+  .attr("stroke","red");*/
+
+//viz.selectAll('path.line')
+//testVis.selectAll('path.line')
+//    .data(tmpData)
+//  .enter().append("svg:path")
+//    .attr("d", d3.svg.line()
+//      .x((d,i) -> x(i))
+//      .y(y))
 
 // load external word data - note asynchrous behavior (parallel requests)
 for (i = 0; i < num_datums; i += chunk_size) {
@@ -63,10 +97,46 @@ function renderVis(newdata) {
 
   var xscale = d3.scale.log()
    .domain([dmin, dmax])
-   .range([0+pad, w-pad])
+   .range([0+xOffset+pad, w-pad])
   var yscale = d3.scale.log()
    .domain([bkmin, bkmax])
-   .range([h-pad, 0+pad])
+   .range([h-yOffset-pad, 0+pad])
+   
+  var axis = viz.selectAll("line")
+  
+  viz.append("svg:line")
+    .attr("x1",xOffset)
+    .attr("y1",0)
+    .attr("x2",xOffset)
+    .attr("y2",h-yOffset)
+    .attr("stroke","red");
+    
+  viz.append("svg:line")
+    .attr("x1",xOffset)
+    .attr("y1",h-yOffset)
+    .attr("x2",w)
+    .attr("y2",h-yOffset)
+    .attr("stroke","red");
+    //.attr("x2",function(d) {return xscale(d);})
+    //.attr("y2",function(d) {return yscale(d);})
+    
+  /*axis.data([100,200,300,400])
+    .enter().append("svg:line")
+    .attr("x1",xscale(0))
+    .attr("y1",yscale(0))
+    .attr("x2",function(d) {return xscale(d);})
+    .attr("y2",function(d) {return yscale(d);})
+    .attr("stroke","red")*/
+  
+  //viz.append("svg:line")
+
+  //var xaxis = d3.svg.axis()
+  //  .scale(xscale)
+  //  .orient("bottom")
+                
+  //var yaxis = d3.svg.axis()
+  //  .scale(yscale)
+  //  .orient("left")
 
   var circle = viz.selectAll("circle")
 
