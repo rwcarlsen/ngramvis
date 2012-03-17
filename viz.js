84ae8528c6ede91e10452c15cfe39eb142b530ff
Eric Alexander <ealexand@cs.wisc.edu>
2012-03-16 19:52:15 -0500

    Have tick labels on the axes. Pretty shoddy, and haven't been able to actually get tick marks yet.

--- viz.js
+++ viz.js
@@ -1,3 +1,19 @@
+/*path {
+    stroke: steelblue;
+    stroke-width: 2;
+    fill: none;
+}
+ 
+line {
+    stroke: black;
+}
+ 
+text {
+    font-family: Arial;
+    font-size: 9pt;
+}*/
+
+
 // global variables
 var h = 750; //height
 var w = 1200; //width
@@ -6,7 +22,6 @@ var rbig = 8; //max radius size - JUST SEEMS TO BE USED TO DEFINE PAD
 var pad = rbig + 10 //padding around the graphing space
 var xOffset = 40
 var yOffset = 40
-//var pad = 300
 
 var data = [];
 var num_datums = 500;
@@ -27,38 +42,6 @@ var viz = d3.select("#viz")
   .append("svg:svg")
   .attr("width", w)
   .attr("height", h);
-
-/*var tmpData = [1,2,3,4,5]
-var testh = 200
-var testw = 500
-var x = d3.scale.linear().domain([0, tmpData.length - 1]).range [0, testw]
-var y = d3.scale.linear().domain([0, d3.max(tmpData)]).range [testh, 0]
-
-var testVis = d3.select("#test")
-  .append("svg:svg")
-  .attr("width",testw)
-  .attr("height",testh);
-
-testVis.append("svg:rect")
-  .attr("x",50)
-  .attr("y",50)
-  .attr("height",50)
-  .attr("width",50);
-  
-viz.append("svg:line")
-  .attr("x1",25)
-  .attr("y1",25)
-  .attr("x2",150)
-  .attr("y2",25)
-  .attr("stroke","red");*/
-
-//viz.selectAll('path.line')
-//testVis.selectAll('path.line')
-//    .data(tmpData)
-//  .enter().append("svg:path")
-//    .attr("d", d3.svg.line()
-//      .x((d,i) -> x(i))
-//      .y(y))
 
 // load external word data - note asynchrous behavior (parallel requests)
 for (i = 0; i < num_datums; i += chunk_size) {
@@ -101,42 +84,64 @@ function renderVis(newdata) {
   var yscale = d3.scale.log()
    .domain([bkmin, bkmax])
    .range([h-yOffset-pad, 0+pad])
-   
-  var axis = viz.selectAll("line")
   
+  // Axes 
   viz.append("svg:line")
     .attr("x1",xOffset)
     .attr("y1",0)
     .attr("x2",xOffset)
     .attr("y2",h-yOffset)
     .attr("stroke","red");
-    
   viz.append("svg:line")
     .attr("x1",xOffset)
     .attr("y1",h-yOffset)
     .attr("x2",w)
     .attr("y2",h-yOffset)
     .attr("stroke","red");
-    //.attr("x2",function(d) {return xscale(d);})
-    //.attr("y2",function(d) {return yscale(d);})
     
-  /*axis.data([100,200,300,400])
-    .enter().append("svg:line")
-    .attr("x1",xscale(0))
-    .attr("y1",yscale(0))
-    .attr("x2",function(d) {return xscale(d);})
-    .attr("y2",function(d) {return yscale(d);})
-    .attr("stroke","red")*/
-  
-  //viz.append("svg:line")
-
-  //var xaxis = d3.svg.axis()
-  //  .scale(xscale)
-  //  .orient("bottom")
-                
-  //var yaxis = d3.svg.axis()
-  //  .scale(yscale)
-  //  .orient("left")
+  // Tick marks on axes
+  viz.selectAll(".xLabel")
+    .data(xscale.ticks(5))
+    .enter().append("svg:text")
+    .attr("class","xLabel")
+    .text(String)
+    .attr("x", function(d) {return xscale(d);})
+    .attr("y", h)
+    .attr("text-anchor", "middle");
+  viz.selectAll(".xTicks")
+    .data(xscale.ticks(5))
+    .enter().append("svg.line")
+    .attr("class", "xTicks")
+    .attr("x1", function(d) {return xscale(d);})
+    .attr("y1", h-yOffset) // probably changing this
+    .attr("x2", function(d) {return xscale(d);})
+    .attr("y2", h-yOffset-10) // probably changing this
+    .attr("stroke", "red");
+    
+  var yLabels = viz.selectAll(".yLabel")
+    .attr("y", function(d) {return yscale(d);})
+    
+  yLabels.data(yscale.ticks(4))
+    .enter().append("svg:text")
+    .attr("class","yLabel")
+    .text(String)
+    .attr("x", 0)
+    .attr("y", function(d) {return yscale(d);})
+    .attr("text-anchor", "right")
+    .attr("dy", 4); //not sure what this line does...
+    
+  var yTicks = viz.selectAll(".yTicks")
+    .attr("y1", function(d) {return yscale(d);})
+    .attr("y2", function(d) {return yscale(d);})
+    
+  yTicks.data(yscale.ticks(4))
+    .enter().append("svg.line")
+    .attr("class", "yTicks")
+    .attr("y1", function(d) {return yscale(d);})
+    .attr("x1", 0) // probably changing this
+    .attr("y2", function(d) {return yscale(d);})
+    .attr("x2", 10) // probably changing this
+    .attr("stroke", "red");
 
   var circle = viz.selectAll("circle")
 
